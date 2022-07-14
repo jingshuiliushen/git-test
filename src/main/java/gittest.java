54dcd3c36829850205bd82c1d1b9d0ae6aa2fe96
jingshuiliushen <yshhsy2006@163.com>
2022-07-14 19:09:21 +0800

    first commit

---
+++ src/main/java/gittest.java
@@ -0,0 +1,11 @@
+/**
+ * @description:
+ * @author: alexyuan
+ * @time: 7/14/22 4:44 PM
+ * @modifiedby:
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
